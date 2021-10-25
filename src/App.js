99f7d06eedb179b9fabca5c95cc9803f5050a99a
aniket-6234 <aniketashok1999@gmail.com>
2021-10-25 16:15:13 +0530

    counter logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+
+
+  const [count, setCount] = useState(0);
+
+
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>COUNTER APPLICATION</h1>
       </header>
+      <div>
+        <h2>the current value is : {count}</h2>
+      </div>
     </div>
+
+    <div className="container">
+      <h1 className="count">{count}</h1>
+      <div className="container">
+        <button onClick={() => setCount(0)}>RESET</button>
+        <button onClick={() => setCount(count + 1)}>INCREMENT</button>
+        <button onClick={() => setCount(count - 1)}>DECREMENT</button>
+      </div>
+    </div>
+    </>
   );
 }
 
